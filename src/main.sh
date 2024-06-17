fb3622aba57d39f60f8d1b9556082bd70cae64c2
Chaipat J. <work.chaipat@gmail.com>
2024-06-17 21:14:01 +0700

    chore ~

--- src/main.sh
+++ src/main.sh
@@ -33,6 +33,25 @@ case ${ARGS[0]} in
   brew_version)
     brew_version
     ;;
+  install)
+    case ${ARGS[1]} in
+      # need python3.10
+      cs111)
+        # check if python3.10 is installed
+        if ! command -v python3.10 &> /dev/null; then
+          echo "Python3.10 is not installed. Install python3.10"
+          brew install python@3.10 || exit 1
+        fi
+        
+        # check if python3.10 is linked
+        if ! brew list python@3.10 &> /dev/null; then
+          echo "Python3.10 is not linked. Link python3.10"
+          brew link python@3.10 || exit 1
+        fi
+      *)
+        echo "Command not found"
+        ;;
+    esac
   *)
     echo "Command not found"
     ;;
